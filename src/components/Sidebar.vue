80633b9ca15301f4b9f31e9158bd5bdd19f5434a
Xin shen <363088847@qq.com>
2021-09-06 22:54:08 +0800

    引入 Less

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="sidebar">
-    <avatar/>
+    <avatar />
     <div class="icons">
       <router-link to="/notebook/" title="笔记"><i class="iconfont icon-note"></i></router-link>
       <router-link to="/notebooks" title="笔记本"><i class="iconfont icon-notebook"></i></router-link>
       <router-link to="/trash" title="回收站"><i class="iconfont icon-trash"></i></router-link>
     </div>
-    <div class="logout">
+    <div class="logout" >
       <i class="iconfont icon-logout"></i>
     </div>
   </div>
@@ -27,27 +27,25 @@ export default {
 </script>
 
 
-<style scoped>
+<style lang="less" scoped>
 #sidebar {
   position: relative;
   width: 56px;
   text-align: center;
   background-color: #2c333c;
 }
-
 .icons {
   margin-top: 15px;
-}
 
-.icons a {
-  padding: 6px 0;
-  display: block;
+  a {
+    padding: 6px 0;
+    display: block;
+  }
 }
 
 .icons .router-link-active {
   background-color: #5e6266;
 }
-
 .logout {
   position: absolute;
   bottom: 20px;
@@ -55,7 +53,6 @@ export default {
   text-align: center;
   cursor: pointer;
 }
-
 .iconfont {
   color: #fff;
 }
