2efd768cecb9e661cf7b5bf1c7edbe74b388b873
Xin shen <363088847@qq.com>
2021-09-06 20:31:47 +0800

    新增路由和页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,41 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from "../components/Login";
+import Notebooks from '@/components/NotebookList.vue'
+import NoteDetail from '@/components/NoteDetail.vue'
+import TrashDetail from '@/components/TrashDetail.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+
+    },
+    {
+        path: '/notebooks',
+        component: Notebooks
+    },
+    {
+        path: '/note/:noteId',
+        component: NoteDetail
+    },
+    {
+        path: '/trash/:noteId',
+        component: TrashDetail
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
