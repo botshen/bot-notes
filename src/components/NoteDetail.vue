4433bb904613f32ea00cceb8051ec05d044c1b57
Xin shen <363088847@qq.com>
2021-09-07 14:58:42 +0800

    未登录时跳转到登录页面

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -1,15 +1,26 @@
 <template>
   <div id="note-detail">
-    <h1>{{ msg }} : {{ $route.params.noteId }}</h1>
+    <h1>{{msg}} : {{ $route.params.noteId }}</h1>
   </div>
 </template>
 
 <script>
+import Auth from '@/apis/auth'
+
 export default {
-  data() {
+  data () {
     return {
-      msg: '笔记详情页'
+      msg: '笔记详情列表'
     }
+  },
+
+  created() {
+    Auth.getInfo()
+        .then(res => {
+          if(!res.isLogin) {
+            this.$router.push({ path: '/login' })
+          }
+        })
   }
 }
 </script>
